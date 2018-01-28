de9607e6919eea516de0ef1f2d846e1f2b33117f
Dmitry <poloabusesuckaz@gmail.com>
2018-01-28 15:13:40 +0200

    trash removed

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -3,18 +3,10 @@ const authRouter = express.Router()
 
 import { login } from '../../controllers/login'
 
-<<<<<<< HEAD
-import { signup } from '../../controllers/signup'
-
-authRouter.post('/login', login)
-
-authRouter.post('/signup', signup)
-=======
 import { register } from '../../controllers/register'
 
 authRouter.post('/login', login)
 
 authRouter.post('/register', register)
->>>>>>> api
 
 export = authRouter
