2b9cdfa6fe88390403b97e88422f18271390279a
Dmitry <poloabusesuckaz@gmail.com>
2018-01-29 21:32:44 +0200

    forget to resolve one conflict

--- src/models/User.ts
+++ src/models/User.ts
@@ -22,14 +22,9 @@ export interface User extends mongoose.Document {
 export const UserSchema = new Schema({
   name: {
     type: String,
-<<<<<<< HEAD
     minlength: 2,
     maxlength: 30,
     required: true
-=======
-    min: 2,
-    max: 30
->>>>>>> api
   },
   surname: {
     type: String,
